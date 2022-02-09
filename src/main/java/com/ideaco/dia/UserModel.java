cdb14920bf400926419964fd9024301ba5cfeb28
putri fatimah <putrifatimah428@gmail.com>
2022-02-09 13:03:16 +0700

    Add user model

---
+++ src/main/java/com/ideaco/dia/UserModel.java
@@ -0,0 +1,82 @@
+package com.ideaco.dia;
+
+import javax.persistence.*;
+
+@Entity
+@Table (name = "tab_user")
+public class UserModel {
+    @Id //primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto-increment
+
+    //column name in mysql table
+    @Column(name = "user_id")
+    private int userId; //variable in java
+    @Column(name = "user_name")
+    private String userName; //string=varchar in sql
+    @Column(name = "user_password")
+    private String userPassword;
+    @Column(name = "user_email")
+    private String userEmail;
+    @Column(name = "user_phone")
+    private String userPhone;
+    @Column(name = "user_address")
+    private String userAddress;
+    @Column(name = "user_resume")
+    private String userResume;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+    public void setUserAddress(String userAddress) {
+        this.userAddress = userAddress;
+    }
+
+    public String getUserResume() {
+        return userResume;
+    }
+
+    public void setUserResume(String userResume) {
+        this.userResume = userResume;
+    }
+}
