e27af87f3cd6f7b7f8e512b8c9ad1183b483cb18
putri fatimah <putrifatimah428@gmail.com>
2022-02-09 13:18:30 +0700

    add new repository

--- src/main/java/com/ideaco/dia/JobRepository.java
+++ src/main/java/com/ideaco/dia/JobRepository.java
@@ -1,7 +1,34 @@
 package com.ideaco.dia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
 // JobModel = class model name , Integer = data type primary key of JobModel
 public interface JobRepository extends JpaRepository<JobModel, Integer> {
+
+    //get object(s) using other parameter
+    Optional<JobModel> findByJobName(String jobName);
+
+    Optional<JobModel> findByJobNameAndJobSalary(String jobName, int salary);
+
+    //get specific data using keywords
+    List<JobModel> findByJobSalaryGreaterThan(int salary);
+
+    //native query
+    @Query(value = "select * from tab_job" + " where job_name like %:jobName%", nativeQuery = true)
+    List<JobModel> searchJob(String jobName);
+
+    //jpql
+    @Query(value = "select j from JobModel j " +
+            "where j.jobSalary >= :salary")
+    List<JobModel> filterJob(@Param("salary") int jobSalary);
+
+    //delete
+    @Transactional
+    void deleteByJobName(String jobName);
 }
