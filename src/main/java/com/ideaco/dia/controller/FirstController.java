5cd8be130c84b8deb0d4f4a017b15bd74b93d536
putri fatimah <putrifatimah428@gmail.com>
2022-02-13 13:00:15 +0700

    add JobWithResponse, all jobs & jobId from JobDTO

--- src/main/java/com/ideaco/dia/controller/FirstController.java
+++ src/main/java/com/ideaco/dia/controller/FirstController.java
@@ -1,7 +1,15 @@
-package com.ideaco.dia;
+package com.ideaco.dia.controller;
 
+import com.ideaco.dia.dto.JobDTO;
+import com.ideaco.dia.model.JobModel;
+import com.ideaco.dia.response.DataResponse;
+import com.ideaco.dia.response.HandlerResponse;
+import com.ideaco.dia.service.FirstService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 @RestController
@@ -9,12 +17,14 @@ import java.util.List;
 
 public class FirstController {
 
-    //new object firstService
+//    define new object without constructor
+    @Autowired
     private FirstService firstService;
 
-    public FirstController(FirstService firstService) {
-        this.firstService = firstService;
-    }
+//    with constructor
+//    public FirstController(FirstService firstService) {
+//        this.firstService = firstService;
+//    }
 
     @GetMapping("/helloWorld")
     public String helloWorld(/*request*/) {
@@ -28,12 +38,13 @@ public class FirstController {
 
     }
 
+//    JobById from JobDTO
     @GetMapping("/job/{jobId}") //{} = path variable
-    public JobModel getJob(@PathVariable("jobId") int jobId) {
+    public JobDTO getJob(@PathVariable("jobId") int jobId) {
         return firstService.getJobById(jobId);
     }
 
-    //get job by name
+//    get job by name
     @GetMapping("/job/name/{jobName}")
     public JobModel getJobByName(@PathVariable("jobName") String jobName) {
         JobModel jobByName = firstService.getJobByName(jobName);
@@ -55,23 +66,25 @@ public class FirstController {
         }
     }
 
-    //get job by specific salary
+//    get job by specific salary
     @GetMapping("/job/salary/{salary}")
     public List<JobModel> getJobBySalary(@PathVariable("salary") int salary) {
         return firstService.getJobBySalary(salary);
     }
 
-    //get all jobs
+//    find all jobs from job dto
     @GetMapping("/jobs")
-    public List<JobModel> getAllJobs() {
+    public List<JobDTO> getAllJobs(){
         return firstService.findAllJobs();
     }
 
+//    search jobs by name
     @GetMapping("/job/search")
     public List<JobModel> searchJob(@RequestParam("jobName") String jobName) {
         return firstService.searchJob(jobName);
     }
 
+//    filter jobs by salary
     @GetMapping("/job/filter")
     public List<JobModel> filterJob(@RequestParam("jobSalary") int jobSalary) {
         return firstService.filterJob(jobSalary);
@@ -125,4 +138,19 @@ public class FirstController {
         return firstService.deleteJobByName(jobName);
     }
 
+    @GetMapping("/job/response")
+    public void getJobWithResponse(HttpServletRequest request, HttpServletResponse response,
+                                   @RequestParam("jobId") int jobId) {
+        JobDTO jobDTO = firstService.getJobById(jobId);
+
+//      standardization error
+        DataResponse<JobDTO> data = new DataResponse<>();
+        data.setData(jobDTO);
+        HandlerResponse.responseSuccessWithData(response, data);
+
+//       HandlerResponse.responseBadRequest(response, "001", "");
+//       HandlerResponse.responseInternalServerError(response, "Something wrong");
+//       HandlerResponse.responseSuccessOK(response,"Success get job");
+//       HandlerResponse.responseUnauthorized(response, "User not authorized");
+    }
 }
