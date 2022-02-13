5739ea407738573054f1ab2bbfeb5cbf4308733a
putri fatimah <putrifatimah428@gmail.com>
2022-02-13 13:09:17 +0700

    add new job dto

---
+++ src/main/java/com/ideaco/dia/dto/JobDTO.java
@@ -0,0 +1,31 @@
+package com.ideaco.dia.dto;
+
+public class JobDTO {
+    private String jobName;
+    private int jobSalary;
+
+    public JobDTO(String jobName, int jobSalary) {
+        this.jobName = jobName;
+        this.jobSalary = jobSalary;
+    }
+    public JobDTO(){
+
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public int getJobSalary() {
+        return jobSalary;
+    }
+
+    public void setJobSalary(int jobSalary) {
+        this.jobSalary = jobSalary;
+    }
+
+    public String getJobName(){
+        return jobName;
+    }
+
+}
